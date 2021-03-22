2bddf32cd3586884534a09346f07c4c0555476ec
Jeff Smith <smith.j.patrick@gmail.com>
2021-03-22 16:18:43 -0400

    new nav update

--- js/navbar.js
+++ js/navbar.js
@@ -1,29 +1,30 @@
 
-      var navToggle = document.querySelector(".nav__toggle");
-      var navMenu = document.querySelector(".nav__menu");
-      var navBurger = document.querySelector(".nav__burger");
-      var navBurgerText = document.querySelector(".nav__toggle-text");
+var nav = document.querySelector(".nav");
+var navToggle = document.querySelector(".nav__toggle");
+var navMenu = document.querySelector(".nav__menu");
+var navBurger = document.querySelector(".nav__burger");
+var navBurgerText = document.querySelector(".nav__toggle-text");
 
-      navToggle.addEventListener("click", function () {
-        if (navMenu.classList.contains("active")) {
-          console.log("remove");
-          navMenu.classList.remove("active");
-          navBurger.classList.remove("close");
-          navBurgerText.innerHTML = "Menu";
-        } else {
-          console.log("add");
-          navMenu.classList.add("active");
-          navBurger.classList.add("close");
-          navBurgerText.innerHTML = "Close";
-        }
-      });
+navToggle.addEventListener("click", function () {
+  if (nav.classList.contains("mobile")) {
+    console.log("remove");
+    nav.classList.remove("mobile");
+    
+    navBurgerText.innerHTML = "Menu";
+  } else {
+    console.log("add");
+    nav.classList.add("mobile");
+    
+    navBurgerText.innerHTML = "Close";
+  }
+});
 
-      var navMenuHorizontal = document.querySelector(".nav__menu");
-      var burgerLabel = document.querySelector(".nav");
+var navMenuHorizontal = document.querySelector(".nav__menu");
+var burgerLabel = document.querySelector(".nav");
 
-      window.addEventListener("resize", function () {
-        if (window.innerWidth > 600) {
-          navMenu.classList.remove("active");
-          navBurgerText.innerHTML = "Menu";
-        }
-      });
+window.addEventListener("resize", function () {
+  if (window.innerWidth > 600) {
+    nav.classList.remove("mobile");
+    navBurgerText.innerHTML = "Menu";
+  }
+});
