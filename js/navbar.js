db27bd8366d38794e8e9d370f8de54008b301f51
Jeff Smith <smith.j.patrick@gmail.com>
2021-04-01 17:38:19 -0400

    updating javascript to change when viewport changes in x/y axes

--- js/navbar.js
+++ js/navbar.js
@@ -1,10 +1,14 @@
 
 var page = document.querySelector("html");
 var nav = document.querySelector(".nav");
+var logoImg = document.querySelector(".nav__img");
 var navToggle = document.querySelector(".nav__toggle");
 var navMenu = document.querySelector(".nav__menu");
 var navBurger = document.querySelector(".nav__burger");
 var navBurgerText = document.querySelector(".nav__toggle-text");
+var w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
 
 navToggle.addEventListener("click", function () {
   if (page.classList.contains("mobile")) {
@@ -20,12 +24,60 @@ navToggle.addEventListener("click", function () {
   }
 });
 
-var navMenuHorizontal = document.querySelector(".nav__menu");
-var burgerLabel = document.querySelector(".nav");
 
 window.addEventListener("resize", function () {
-  if (window.innerWidth > 600) {
+w = window.innerWidth
+|| document.documentElement.clientWidth
+|| document.body.clientWidth;
+  if (w > 600) {
     page.classList.remove("mobile");
     navBurgerText.innerHTML = "Menu";
+    nav.classList.remove("hidden-nav");
+  } 
+})
+
+
+/////////////////////
+//Scrolling
+/////////////////////
+var navHide = 100; //scroll distance that nav hides
+
+window.onscroll = function() {
+    console.log("screen-width: " + w)
+    myFunction();
+}
+
+function myFunction() {
+  
+    if (document.body.scrollTop > navHide || document.documentElement.scrollTop > navHide) {
+      logoImg.classList.add("nav__img--small");
+      nav.classList.add("nav--scrolled");
+    } else {
+      nav.classList.remove("nav--scrolled");
+      logoImg.classList.remove("nav__img--small");
+      console.log("scrolled up")
+    }
+  };
+
+
+var lastScrollTop = 0;
+
+// element should be replaced with the actual target element on which you have applied scroll, use window in case of no target element.
+window.addEventListener("scroll", function(){ // or window.addEventListener("scroll"....
+
+   var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
+
+   if ((w <= 600)) {
+   if (st > lastScrollTop && st > navHide ){
+    // downscroll code 
+
+    nav.classList.add("hidden-nav");
+
+  } else {
+      // upscroll code
+      nav.classList.remove("hidden-nav");
+   }
   }
-});
+   lastScrollTop = st <= 0 ? 0 : st; // For Mobile or negative scrolling
+}, false);
+
